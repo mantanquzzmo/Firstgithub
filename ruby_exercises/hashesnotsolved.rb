b2ae117859b400d8c99a9f1df90ff3213ad15fcb
mantanquzzmo <quzzmo@hotmail.com>
2019-11-04 14:25:07 +0100

    updated my_group

--- ruby_exercises/hashesnotsolved.rb
+++ ruby_exercises/hashesnotsolved.rb
@@ -1,6 +1,2 @@
-family = { uncles: ["bob", "joe", "steve"],
-          sisters: ["jane", "jill", "beth"],
-          brothers: ["frank", "rob", "david"],
-          aunts: ["mary", "sally", "susan"]
-          }
-#brothers = family.select {|v| k == :brothers || :sisters}
+family = { uncles: ["bob", "joe", "steve"], sisters: ["jane", "jill", "beth"], brothers: ["frank", "rob", "david"], aunts: ["mary", "sally", "susan"] }
+closefamily = family.select {|v| k == :brothers || k == :sisters}
--- ruby_exercises/my_group.rb
+++ ruby_exercises/my_group.rb
@@ -1,5 +1,5 @@
 my_group = Array.new
-person = {name: "person_1", gender: "m", age: 20}
-person = {name: "person_2", gender: "f", age: 22}
-person_3 = {name: "person_3", gender: "m", age: 24}
+person_1 = {name: "john", gender: "m", age: 20}
+person_2 = {name: "jake", gender: "f", age: 22}
+person_3 = {name: "jill", gender: "m", age: 24}
 my_group = ["person_1", "person_2", "person_3"]
